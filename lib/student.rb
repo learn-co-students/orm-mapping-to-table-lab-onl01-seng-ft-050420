862888c90b88f9caa4b848dad0aa5e74248dd0f6
github username <github email address>
2020-05-22 21:45:05 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -11,41 +11,36 @@ class Student
   end 
 
   def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade INTEGER
-    );
-    SQL 
+    sql = 
+    "CREATE TABLE IF NOT EXISTS students (" +
+    "id INTEGER PRIMARY KEY," +
+    "name TEXT," +
+    "grade TEXT" +
+    ");"
 
     DB[:conn].execute(sql)
   end
 
   def self.drop_table
-    sql = <<-SQL 
-    DROP TABLE students
-    SQL 
+    sql = "DROP TABLE students"
 
     DB[:conn].execute(sql)
   end 
 
   def save
-    sql = <<-SQL 
-    INSERT INTO students (name, grade)
-    VALUES (?, ?)
-    SQL
+    sql =  
+    "INSERT INTO students (name, grade)" +
+    "VALUES (?, ?)"
 
     DB[:conn].execute(sql, self.name, self.grade)
-    binding.pry
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
 
-  def self.create(name, grade)
+  def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
-    studnet
+    student
   end
   
 end
